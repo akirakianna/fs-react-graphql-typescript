213f16789508389169af7c0ea84e4bfb4958e313
akirakianna <kianna.akiralove@gmail.com>
2020-08-25 19:58:18 +0100

    CRUD for GraphQL

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,29 +1,65 @@
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql'
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql'
 import { Post } from '../entities/Post'
 import { MyContext } from 'src/types'
 
+
+//* CRUD through GraphQL *//
+
 @Resolver()
 export class PostResolver {
+
+  //* Return all posts
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {})
   }
-  //! Query is for gettin data
+
+  //* Get a single post by id
+  //! Query is for getting data
   @Query(() => Post, { nullable: true })
   post(
     //! id is TypeScript type, GraphQL type is Int.
-    @Arg('id', () => Int) id: number,
+    @Arg('id') id: number,
     @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { id })
   }
 
+  //* Create a new post
   //!Mutation is for changing (changing on the server, e.g create, edit, delete)
   @Mutation(() => Post)
   async createPost(
-    @Arg('title', () => String) title: string,
+    @Arg('title') title: string,
     @Ctx() { em }: MyContext): Promise<Post> {
     const post = em.create(Post, { title })
     await em.persistAndFlush(post)
     return post
+  }
+
+  //* Edit a post
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id })
+    if (!post) {
+      return null
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title
+      await em.persistAndFlush(post)
+    }
+    return post
+  }
+
+  //* Delete a post
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id })
+    return true
   }
 }
